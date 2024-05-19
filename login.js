d02d14d650e6de80e06b310b3a6ede407728dc7e
AleGod125 <alejandroviolori@gmail.com>
2024-05-18 22:02:25 -0500

    Errores login

--- login.js
+++ login.js
@@ -17,30 +17,51 @@ function redirigir() {
                 console.error('Error al obtener la lista de posts:', error);
             });
 
-            function login() {
-                const email = document.getElementById('email').value;
-                const password = document.getElementById('password').value;
-            
-                axios.post(`http://127.0.0.1:8000/login?email=${email}&password=${password}`)
+            axios.get('http://127.0.0.1:8000/getUser')
+    .then(function (response) {
+        const posts = response.data;
+        const postListElement = document.getElementById('post-list');
+
+        posts.forEach(function (post) {
+            const listItem = document.createElement('li');
+            listItem.textContent = post.Nombre;
+            postListElement.appendChild(listItem);
+        });
+    })
+    .catch(function (error) {
+        console.error('Error al obtener la lista de posts:', error);
+    });
+
+function login() {
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const emailValue = email.value;
+    const passwordValue = password.value;
+
+    axios.post(`http://127.0.0.1:8000/login?email=${emailValue}&password=${passwordValue}`)
+        .then(function (response) {
+            console.log(response.data);
+            if (response.data.message === 'Usuario Iniciado exitosamente') {
+                axios.get(`http://127.0.0.1:8000/getID?email=${emailValue}&password=${passwordValue}`)
                     .then(function (response) {
                         console.log(response.data);
-                        if (response.data.message === 'Usuario Iniciado exitosamente') {
-                            axios.get(`http://127.0.0.1:8000/getID?email=${email}&password=${password}`)
-                                .then(function (response) {
-                                    console.log(response.data);
-                                    const userData = response.data[0];
-                                    const id = userData.UserID
-                                    window.location.href = `profile.html?id=${id}`;
-                                })
-                                .catch(function (error) {
-                                    console.error('Error al obtener el ID del usuario:', error);
-                                });
-                        } else {
-                            alert('Credenciales inválidas');
-                        }
+                        const userData = response.data[0];
+                        const id = userData.UserID;
+                        window.location.href = `profile.html?id=${id}`;
                     })
                     .catch(function (error) {
-                        console.error('Error al iniciar sesión:', error);
-                        alert('Credenciales inválidas');
+                        console.error('Error al obtener el ID del usuario:', error);
                     });
-            }    
+            } else {
+                email.style.border = "2px solid red";
+                password.style.border = "2px solid red";
+               document.getElementById("error").style.display = "block";
+            }
+        })
+        .catch(function (error) {
+            console.error('Error al iniciar sesión:', error);
+            document.getElementById("error").style.display = "block";
+            email.style.border = "2px solid red";
+            password.style.border = "2px solid red";
+        });
+}
